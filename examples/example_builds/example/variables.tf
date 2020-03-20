6d6255307edcd2f9ab4d32e4dbd6facaa8641df4
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-03-20 00:14:58 -0400

    Added check to ensure that variable type is not quoted

--- examples/example_builds/example/variables.tf
+++ examples/example_builds/example/variables.tf
@@ -1,16 +1,16 @@
 # Variable do_region config
 variable "do_region" {
-    type = "string"
+    type = string
     description = "DigitalOcean region"
     default = "nyc1"
 }# Variable do_ssh_keys config
 variable "do_ssh_keys" {
-    type = "string"
+    type = string
     description = "DigitalOcean SSH keys to deploy to new droplets"
     default = "[]"
 }# Variable do_token config
 variable "do_token" {
-    type = "string"
+    type = string
     description = "DigitialOcean token"
     default = ""
 }
