827f9af8dc250085648ccc3e705f4ffdc6b1c029
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-03-24 17:33:51 -0400

    Updated AzureRM resources format
    These changes align AzureRM current state to vSphere resources
    structure. Add'l functionality will come over time.
    
    Closes #6

--- examples/example_builds/example/environments/staging/main.tf
+++ examples/example_builds/example/environments/staging/main.tf
@@ -13,11 +13,4 @@ module "network" {
 module "services" {
     source      = "../../modules/services"
     environment = "staging"
-    azurerm_domain = ""
-    azurerm_environment = "public"
-    azurerm_features = "{}"
-    azurerm_image_reference = "{'ubuntu-16-04-x64': {'publisher': 'Canonical', 'offer': 'UbuntuServer', 'sku': '16.04-LTS', 'version': 'latest'}, 'ubuntu-18-04-x64': {'publisher': 'Canonical', 'offer': 'UbuntuServer', 'sku': '18.04-LTS', 'version': 'latest'}}"
-    azurerm_location = "East US"
-    azurerm_subscription_id = ""
-    azurerm_tenant_id = ""
 }
--- examples/example_builds/example/modules/network/resources.tf
+++ examples/example_builds/example/modules/network/resources.tf
@@ -2,7 +2,7 @@
 # Resource DigitalOcean virtual machine
 resource "digitalocean_droplet" "test_do_network" {
   count  = 1
-  name   = format("test-do-network-%02s-%s", count.index + 1, substr(var.environment,0,4))
+  name   = format("test-do-network-%02s-%s", count.index + 1, substr(var.environment, 0, 4))
   image  = "ubuntu-18-04-x64"
   region = var.do_region
   size   = "s-1vcpu-1gb"
