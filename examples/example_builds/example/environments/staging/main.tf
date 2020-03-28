15fe83284dcef730ea01caddef2f26c9985ea386
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-03-28 17:42:33 -0400

    Changed modules to include environment as part of their name
    This will ensure that there is not any confusion over which environment.
    We also still include the environment var as part of the module.
    But this will ensure we follow best practices.

--- examples/example_builds/example/environments/staging/main.tf
+++ examples/example_builds/example/environments/staging/main.tf
@@ -1,22 +1,22 @@
 # Generated using https://github.com/mrlesmithjr/terraform-builder
-# Module root config
-module "root" {
+# Module staging-root config
+module "staging-root" {
   source      = "../../root"
   environment = "staging"
   azurerm_location   = "North Central US"
   do_domain   = "stg.example.org"
   do_region   = "ams3"
 }
-# Module network config
-module "network" {
+# Module staging-network config
+module "staging-network" {
   source      = "../../modules/network"
   environment = "staging"
   azurerm_location   = "North Central US"
   do_domain   = "stg.example.org"
   do_region   = "ams3"
 }
-# Module services config
-module "services" {
+# Module staging-services config
+module "staging-services" {
   source      = "../../modules/services"
   environment = "staging"
   azurerm_location   = "North Central US"
