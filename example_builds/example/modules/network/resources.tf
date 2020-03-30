3025da08f21b33f976fdc835b6d4ea81a6fa8d5c
Larry Smith Jr. <mrlesmithjr@gmail.com>
2020-03-29 23:01:01 -0500

    Deployed 4d99a96 with MkDocs version: 1.1

--- example_builds/example/modules/network/resources.tf
+++ example_builds/example/modules/network/resources.tf
@@ -1,1 +1,17 @@
 # Generated using https://github.com/mrlesmithjr/terraform-builder
+# Resource DigitalOcean tag
+resource "digitalocean_tag" "default_firewall" {
+  name = "default-firewall"
+}
+# Resource DigitalOcean tag
+resource "digitalocean_tag" "default_firewall_env" {
+  name = format("default-firewall-%s", var.environment)
+}
+# Resource DigitalOcean tag
+resource "digitalocean_tag" "example_digitalocean" {
+  name = "example-digitalocean"
+}
+# Resource DigitalOcean tag
+resource "digitalocean_tag" "example_digitalocean_env" {
+  name = format("example-digitalocean-%s", var.environment)
+}
