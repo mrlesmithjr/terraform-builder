f33b402966c44be37fd392606d1a13ef14bc1df4
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-06-18 08:39:13 -0400

    Cleaned up DigitalOcean templates
    - DigitalOcean tags were getting added in places where they were not
    needed. So, these have been cleaned up.
    - Removed duplicated logic found
    - Cleaned up formatting

--- examples/example_builds/example/modules/services/resources.tf
+++ examples/example_builds/example/modules/services/resources.tf
@@ -1,25 +1,8 @@
 # Generated using https://github.com/mrlesmithjr/terraform-builder
 # Resource DigitalOcean tag
-resource "digitalocean_tag" "default_firewall" {
-  name = "default-firewall"
-}
-# Resource DigitalOcean tag
-resource "digitalocean_tag" "default_firewall_env" {
-  name = format("default-firewall-%s", var.environment)
-}
-# Resource DigitalOcean tag
-resource "digitalocean_tag" "example_digitalocean" {
-  name = "example-digitalocean"
-}
-# Resource DigitalOcean tag
-resource "digitalocean_tag" "example_digitalocean_env" {
-  name = format("example-digitalocean-%s", var.environment)
-}
-# Resource DigitalOcean tag
 resource "digitalocean_tag" "env" {
   name = format("%s", var.environment)
 }
-
 # Data vSphere tag category
 data "vsphere_tag_category" "environment" {
   name = format("%s", var.environment)
